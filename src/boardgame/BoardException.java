7c09fb2d1ca39f25d43cdd9ca909b17f469e0a5f
Victor Melo <melovictor515@gmail.com>
2022-04-07 00:01:00 -0300

    BoardException and defensive programming

---
+++ src/boardgame/BoardException.java
@@ -0,0 +1,10 @@
+package boardgame;
+
+public class BoardException extends RuntimeException {
+    private static final long serilVersionUID = 1L;
+
+    public BoardException(String msg) {
+        super(msg);
+    }
+
+}
